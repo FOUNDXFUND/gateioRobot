65c31399f4dfa8e6fc163a8e878883fa36d1fd2d
Hao Jiang <362380376@qq.com>
2018-05-07 03:20:46 +0800

    😜😜😜

--- service/quant.go
+++ service/quant.go
@@ -10,7 +10,17 @@ import (
 const STATUS_OPEN = "open"
 const STATUS_CANCELLED = "cancelled"
 const STATUS_DONW  = "done"
+const BUY_TYPE = "buy"
+const SELL_TYPE = "sell"
 
+var Watching *WatchingOrderInfo
+
+
+// 当前监控订单信息
+type WatchingOrderInfo struct {
+	OrderNum string
+	Type string // 买卖类型, sell卖出， buy买入
+}
 
 
 func SwitchWathchChecking() {
@@ -29,10 +39,27 @@ func OderStatusChecking(myOrders *lib.OpenOrders) {
 		return
 	}
 
+	if Watching == nil {
+		return
+	}
 
 	for _,v := range myOrders.Orders{
+
+		// 如果当前订单是买单 --> 计算最佳挂卖单价格 --> 挂卖单
+		if Watching.Type == BUY_TYPE {
+
+		}
+
+		// 如果当前订单是卖单 --> 计算最佳挂买单价格 --> 挂买单
+		if Watching.Type == SELL_TYPE {
+
+		}
+
 		fmt.Println("组合:", v.CurrencyPair, " 状态", v.Status)
 	}
+}
+
+func CalcBestPrice(order *lib.Oders) {
 
 }
 
